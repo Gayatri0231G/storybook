58a5d93dbd6989d3838771bc6d6bb9fee15fb0eb
Gouda12345 <gouda8051@gmail.com>
2025-02-10 15:59:52 +0530

    first commit

--- my-react-app/src/Components/BillRecharge.jsx
+++ my-react-app/src/Components/BillRecharge.jsx
@@ -4,20 +4,20 @@ import { FaPlane, FaBus, FaTrain, FaHotel, FaCar } from "react-icons/fa6";
 import "./BillRecharge.css";
 
 const billServices = [
-  { name: "Mobile", icon: <FaMobileAlt style={{ color: "#ff6600" }} /> }, // Orange
-  { name: "Electricity", icon: <FaLightbulb style={{ color: "#ffd700" }} /> }, // Yellow
-  { name: "DTH", icon: <FaSatelliteDish style={{ color: "#800080" }} /> }, // Purple
-  { name: "Water", icon: <FaTint style={{ color: "#0088ff" }} /> }, // Blue
-  { name: "Gas", icon: <FaGasPump style={{ color: "#ff4500" }} /> }, // Red-Orange
-  { name: "Insurance", icon: <FaUmbrella style={{ color: "#006400" }} /> }, // Green
+  { name: "Mobile", icon: <FaMobileAlt style={{ color: "#ff6600" }} /> }, 
+  { name: "Electricity", icon: <FaLightbulb style={{ color: "#ffd700" }} /> }, 
+  { name: "DTH", icon: <FaSatelliteDish style={{ color: "#800080" }} /> }, 
+  { name: "Water", icon: <FaTint style={{ color: "#0088ff" }} /> }, 
+  { name: "Gas", icon: <FaGasPump style={{ color: "#ff4500" }} /> }, 
+  { name: "Insurance", icon: <FaUmbrella style={{ color: "#006400" }} /> }, 
 ];
 
 const travelServices = [
-  { name: "Flight", icon: <FaPlane style={{ color: "#ff0000" }} />, extra: "Powered by Easemytrip.com" }, // Red
-  { name: "Bus", icon: <FaBus style={{ color: "#228b22" }} />, extra: "Affordable Rides" }, // Green
-  { name: "Train", icon: <FaTrain style={{ color: "#8b4513" }} /> }, // Brown
-  { name: "Hotel", icon: <FaHotel style={{ color: "#4b0082" }} />, extra: "Budget-friendly Stay" }, // Indigo
-  { name: "Car Rentals", icon: <FaCar style={{ color: "#4682b4" }} />, extra: "Drive Easy Anywhere" }, // Steel Blue
+  { name: "Flight", icon: <FaPlane style={{ color: "#ff0000" }} />, extra: "Powered by Easemytrip.com" }, 
+  { name: "Bus", icon: <FaBus style={{ color: "#228b22" }} />, extra: "Affordable Rides" }, 
+  { name: "Train", icon: <FaTrain style={{ color: "#8b4513" }} /> }, 
+  { name: "Hotel", icon: <FaHotel style={{ color: "#4b0082" }} />, extra: "Budget-friendly Stay" }, 
+  { name: "Car Rentals", icon: <FaCar style={{ color: "#4682b4" }} />, extra: "Drive Easy Anywhere" }, 
 ];
 
 const Services = () => {
@@ -30,7 +30,7 @@ const Services = () => {
             <p className="description">Pay your bills & recharge instantly with Justdial</p>
             <h5>Explore More</h5>
           </div>
-          <div className="icon-grid">
+          <div className="icon-grid1">
             {billServices.map((item, idx) => (
               <div key={idx} className="service-item">
                 <div className="icon-box">
@@ -41,11 +41,8 @@ const Services = () => {
             ))}
           </div>
         </div>
-
-        {/* Center Horizontal Line */}
         <div className="divider"></div>
 
-        {/* Travel Bookings Section */}
         <div className="section">
           <div className="left-content">
             <h2 className="section-title">Travel Bookings</h2>
